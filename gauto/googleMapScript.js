c24cc7e30a3e84d76c3abb3fa0b103a33aefc0db
galiak <galiak@wix.com>
2016-03-30 16:22:36 +0300

    code refactoring

---
+++ gauto/googleMapScript.js
@@ -0,0 +1,144 @@
+initAutoMap = function(data) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13
+    });
+
+    if(!document.getElementById('pac-input')) {
+        var text = '<input id="pac-input" class="controls" type="text" placeholder="Enter location">';
+        text += '<div id="type-selector" class="controls">';
+        text +=	'<input type="radio" name="type" id="changetype-all" checked="checked">';
+        text +=	'<label class="shortLabel" for="changetype-all">All</label>';
+        text +=	'<input type="radio" name="type" id="changetype-establishment">';
+        text +=	'<label class="shortLabel" for="changetype-establishment">Establishments</label>';
+        text +=	'<input type="radio" name="type" id="changetype-address">';
+        text +=	'<label class="shortLabel" for="changetype-address">Addresses</label>';
+        text +=	'<input type="radio" name="type" id="changetype-geocode">';
+        text +=	'<label class="shortLabel" for="changetype-geocode">Geocodes</label>';
+        text +=	'</div>	';
+        $(text).appendTo($('.mapContainer'));
+    }
+
+    var input = document.getElementById('pac-input');
+
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    function geoChange(data) {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+        data.searchParams.address = place;
+        data.searchParams.name = place.name;
+    }
+
+    autocomplete.addListener('place_changed', geoChange.bind(null, data), false);
+
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+        var radioButton = document.getElementById(id);
+        radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+        });
+    }
+
+    setupClickListener('changetype-all', []);
+    setupClickListener('changetype-address', ['address']);
+    setupClickListener('changetype-establishment', ['establishment']);
+    setupClickListener('changetype-geocode', ['geocode']);
+};
+
+
+initRadiusMap = function(data) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {
+            lat: typeof(data.searchParams.address.geometry.location.lat) === 'function' ? data.searchParams.address.geometry.location.lat() : data.searchParams.address.geometry.location.lat,
+            lng: typeof(data.searchParams.address.geometry.location.lng) === 'function' ? data.searchParams.address.geometry.location.lng() : data.searchParams.address.geometry.location.lng
+        },
+        zoomControl: true
+    });
+    var marker = new google.maps.Marker({
+        position: {
+            lat: typeof(data.searchParams.address.geometry.location.lat) === 'function' ? data.searchParams.address.geometry.location.lat() : data.searchParams.address.geometry.location.lat,
+            lng: typeof(data.searchParams.address.geometry.location.lng) === 'function' ? data.searchParams.address.geometry.location.lng() : data.searchParams.address.geometry.location.lng
+        },
+        map: map
+    });
+    var circle = new google.maps.Circle({
+        map: map,
+        radius: data.searchParams.address.geometry.radius,
+        fillColor: '#2E86C1',
+        strokeColor: '#2E86C1',
+        strokeWeight: 1
+    });
+    circle.bindTo('center', marker, 'position');
+
+    var geocoder = new google.maps.Geocoder();
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map, data);
+    });
+};
+
+geocodeAddress = function(geocoder, resultsMap, data) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            data.searchParams.address = results[0];
+            data.searchParams.address.geometry.radius = getRadius(data);
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+            var circle = new google.maps.Circle({
+                map: resultsMap,
+                radius: data.searchParams.address.geometry.radius,
+                fillColor: '#2E86C1',
+                strokeColor: '#2E86C1',
+                strokeWeight: 1
+            });
+            circle.bindTo('center', marker, 'position');
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+};
--- gauto/script.js
+++ gauto/script.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 		$('.navigation').empty();
 		$('#content').empty();
 		getPlatforms(data);
-		if($('input[name=search]:checked').val() === 'glocation') {
+		if ($('input[name=search]:checked').val() === 'glocation') {
 			getName(data);
 			getRadius(data);
 		}
@@ -38,7 +38,9 @@ $(document).ready(function(){
 			data: JSON.stringify(data),
 			contentType: 'application/json; charset=utf-8',
 			dataType: 'json',
-			success: function(data){parseResults(data); $('.loading').hide();}
+			success: function(data){
+				parseResults(data); $('.loading').hide();
+			}
 		});
 	});
 });
@@ -57,12 +59,15 @@ searchByUrls = function(urls) {
 		data: JSON.stringify(data),
 		contentType: 'application/json; charset=utf-8',
 		dataType: 'json',
-		success: function(data){parsePlatformData(data)}
+		success: function(data){
+			parsePlatformData(data)
+		}
 	});
 };
 
 parseUnifiedGeneralInfo = function(generalInfo) {
 	var content = '<ul>';
+
 	generalInfo.site_title ? content += '<li>Site Title [' + generalInfo.site_title.source + ']: <samp>' + generalInfo.site_title.value + '</samp></li>' : '';
 	generalInfo.category ? content += '<li>Category [' + generalInfo.category.source + ']: <samp>' + generalInfo.category.value + '</samp></li>' : '';
 	generalInfo.sub_category ? content += '<li>Sub-Category [' + generalInfo.sub_category.source + ']: <samp>' + parseSubCategory(generalInfo.sub_category.value) + '</samp></li>' : '';
@@ -89,6 +94,7 @@ parseUnifiedGeneralInfo = function(generalInfo) {
 
 parseGeneralInfo = function(generalInfo) {
 	var content = '<ul>';
+
 	generalInfo.site_title ? content += '<li>Site Title: <samp>' + generalInfo.site_title + '</samp></li>' : '';
 	generalInfo.category ? content += '<li>Category: <samp>' + generalInfo.category + '</samp></li>' : '';
 	generalInfo.sub_category ? content += '<li>Sub-Category: <samp>' + parseSubCategory(generalInfo.sub_category) + '</samp></li>' : '';
@@ -103,6 +109,7 @@ parseGeneralInfo = function(generalInfo) {
 
 parseUnifiedContactDetails = function(contactDetails) {
 	var content = '<ul>';
+
 	contactDetails.address ? content += '<li>Address: <samp>' + contactDetails.address.full_addr + '</samp></li>' : '';
 	contactDetails.phone ? content += '<li>Phone [' + contactDetails.phone.source + ']: <samp>' + contactDetails.phone.value + '</samp></li>' : '';
 	contactDetails.email ? content += '<li>Email ['+ contactDetails.email.source + ']: <samp>' + contactDetails.email.value + '</samp></li>' : '';
@@ -120,6 +127,7 @@ parseOpenHours = function(openHours) {
 		content += '<li>* ' + i.day + ': ' + i.from_hour + '-' + i.to_hour + ' *</li>';
 	});
 	content += '</ul>';
+
 	return content;
 };
 
@@ -130,11 +138,13 @@ parseSubCategory= function(subCategory) {
 		content += '<li>* ' + i.name + ' *</li>';
 	});
 	content += '</ul>';
+
 	return content;
 };
 
 parseContactDetails = function(contactDetails) {
 	var content = '<ul>';
+
 	contactDetails.address ? content += '<li>Address: <samp>' + contactDetails.address.full_addr + '</samp></li>' : '';
 	contactDetails.phone ? content += '<li>Phone: <samp>' + contactDetails.phone + '</samp></li>' : '';
 	contactDetails.email ? content += '<li>Email: <samp>' + contactDetails.email + '</samp></li>' : '';
@@ -161,6 +171,7 @@ parseWebsite = function(website) {
 
 parseUnifiedFeeds = function(feeds){
 	var content = '<h4>' + feeds.source + ': </h4>';
+
 	content += '<ul class="feeds">';
 	_(feeds.value).forEach(function(n) {
 		var time = new Date(n.created_time);
@@ -173,6 +184,7 @@ parseUnifiedFeeds = function(feeds){
 
 parseUnifiedEvents = function(events){
 	var content = '<h4>' + events.source + ': </h4>';
+
 	content += '<ul class="events">';
 	_(events.value).forEach(function(n) {
 		content += '<li class="item">Name: <samp>' + n.name + '</samp><br /> Start Time: <samp>' + n.start_time + '</samp><br /> + Description: <samp>' + n.description + '</samp><br /><img src="' + n.image_url + '"/></li>';
@@ -184,6 +196,7 @@ parseUnifiedEvents = function(events){
 
 parseUnifiedReviews = function(reviews){
 	var content = '<h4>' + reviews.source + ': </h4>';
+
 	content += '<ul class="reviews">';
 	_(reviews.value).forEach(function(n) {
 		var time = new Date(n.time);
@@ -196,6 +209,7 @@ parseUnifiedReviews = function(reviews){
 
 parseReviews = function(reviews){
 	var content = '<ul class="reviews">';
+
 	_(reviews).forEach(function(n) {
 		var time = new Date(n.time);
 		content += '<li class="item">Text: <samp>' + n.text + '</samp><br />Created: <samp>' + time + '</samp><br /> Author: <samp>' + n.author_name + '</samp>' + (n.author_photo ? '<img src="https:' + n.author_photo + '" />' : '') + '</li>';
@@ -238,6 +252,7 @@ parseFacebookPhotos = function(photos) {
 parsePhotos = function(photoAlbums) {
 	if (_(photoAlbums.photos)) {
 		var content = '<ul class="photos">';
+
 		_(photoAlbums.photos).forEach(function(n) {
 			content += '<li>' + n.description + '</li>';
 			content += '<li><img src="' + n.photo_url + '" /></li>';
@@ -270,7 +285,8 @@ parsePlatformData = function(answer) {
 
 	_.forEach(answer.platforms, function(i) {
 		content += '<table class="details">';
-		if(i.platform_name === 'unified_fields') {
+
+		if (i.platform_name === 'unified_fields') {
 			content += '<caption><span class="icon response"></span>' + i.platform_name + '</caption>';
 			content += '<tr><th>General Info</th>';
 			i.general_info ? content += '<td>' + parseUnifiedGeneralInfo(i.general_info) + '</td></tr>' : content += emptyCell;
@@ -373,150 +389,6 @@ displayResultData = function(data) {
 	$('#resultData').jsonViewer(data);
 };
 
-initAutoMap = function(data) {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -33.8688, lng: 151.2195},
-		zoom: 13
-	});
-
-	if(!document.getElementById('pac-input')) {
-		var text = '<input id="pac-input" class="controls" type="text" placeholder="Enter location">';
-		text += '<div id="type-selector" class="controls">';
-		text +=	'<input type="radio" name="type" id="changetype-all" checked="checked">';
-		text +=	'<label class="shortLabel" for="changetype-all">All</label>';
-		text +=	'<input type="radio" name="type" id="changetype-establishment">';
-		text +=	'<label class="shortLabel" for="changetype-establishment">Establishments</label>';
-		text +=	'<input type="radio" name="type" id="changetype-address">';
-		text +=	'<label class="shortLabel" for="changetype-address">Addresses</label>';
-		text +=	'<input type="radio" name="type" id="changetype-geocode">';
-		text +=	'<label class="shortLabel" for="changetype-geocode">Geocodes</label>';
-		text +=	'</div>	';
-		$(text).appendTo($('.mapContainer'));
-	}
-
-	var input = document.getElementById('pac-input');
-
-	var types = document.getElementById('type-selector');
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-
-	var autocomplete = new google.maps.places.Autocomplete(input);
-	autocomplete.bindTo('bounds', map);
-
-	var infowindow = new google.maps.InfoWindow();
-	var marker = new google.maps.Marker({
-		map: map,
-		anchorPoint: new google.maps.Point(0, -29)
-	});
-
-	function doThis(data) {
-		infowindow.close();
-		marker.setVisible(false);
-		var place = autocomplete.getPlace();
-		if (!place.geometry) {
-			window.alert("Autocomplete's returned place contains no geometry");
-			return;
-		}
-
-		// If the place has a geometry, then present it on a map.
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(17);  // Why 17? Because it looks good.
-		}
-
-		marker.setPosition(place.geometry.location);
-		marker.setVisible(true);
-
-		var address = '';
-		if (place.address_components) {
-			address = [
-				(place.address_components[0] && place.address_components[0].short_name || ''),
-				(place.address_components[1] && place.address_components[1].short_name || ''),
-				(place.address_components[2] && place.address_components[2].short_name || '')
-			].join(' ');
-		}
-
-		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-		infowindow.open(map, marker);
-		data.searchParams.address = place;
-		data.searchParams.name = place.name;
-	}
-
-	autocomplete.addListener('place_changed', doThis.bind(null, data), false);
-
-	// Sets a listener on a radio button to change the filter type on Places
-	// Autocomplete.
-	function setupClickListener(id, types) {
-		var radioButton = document.getElementById(id);
-		radioButton.addEventListener('click', function() {
-			autocomplete.setTypes(types);
-		});
-	}
-
-	setupClickListener('changetype-all', []);
-	setupClickListener('changetype-address', ['address']);
-	setupClickListener('changetype-establishment', ['establishment']);
-	setupClickListener('changetype-geocode', ['geocode']);
-};
-
-initRadiusMap = function(data) {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 10,
-		center: {
-			lat: typeof(data.searchParams.address.geometry.location.lat) === 'function' ? data.searchParams.address.geometry.location.lat() : data.searchParams.address.geometry.location.lat,
-			lng: typeof(data.searchParams.address.geometry.location.lng) === 'function' ? data.searchParams.address.geometry.location.lng() : data.searchParams.address.geometry.location.lng
-		},
-		zoomControl: true
-	});
-	var marker = new google.maps.Marker({
-		position: {
-			lat: typeof(data.searchParams.address.geometry.location.lat) === 'function' ? data.searchParams.address.geometry.location.lat() : data.searchParams.address.geometry.location.lat,
-			lng: typeof(data.searchParams.address.geometry.location.lng) === 'function' ? data.searchParams.address.geometry.location.lng() : data.searchParams.address.geometry.location.lng
-		},
-		map: map
-	});
-	var circle = new google.maps.Circle({
-		map: map,
-		radius: data.searchParams.address.geometry.radius,
-		fillColor: '#2E86C1',
-		strokeColor: '#2E86C1',
-		strokeWeight: 1
-	});
-	circle.bindTo('center', marker, 'position');
-
-	var geocoder = new google.maps.Geocoder();
-	document.getElementById('submit').addEventListener('click', function() {
-		geocodeAddress(geocoder, map, data);
-	});
-};
-
-geocodeAddress = function(geocoder, resultsMap, data) {
-	var address = document.getElementById('address').value;
-	geocoder.geocode({'address': address}, function(results, status) {
-		if (status === google.maps.GeocoderStatus.OK) {
-			data.searchParams.address = results[0];
-			data.searchParams.address.geometry.radius = getRadius(data);
-			resultsMap.setCenter(results[0].geometry.location);
-			var marker = new google.maps.Marker({
-				map: resultsMap,
-				position: results[0].geometry.location
-			});
-			var circle = new google.maps.Circle({
-				map: resultsMap,
-				radius: data.searchParams.address.geometry.radius,
-				fillColor: '#2E86C1',
-				strokeColor: '#2E86C1',
-				strokeWeight: 1
-			});
-			circle.bindTo('center', marker, 'position');
-		} else {
-			alert('Geocode was not successful for the following reason: ' + status);
-		}
-	});
-};
-
 unifiedPlatforms = function(mergedResults) {
 	var platforms = '',
 		sorted = _.sortBy(mergedResults, 'platform_name');
@@ -529,8 +401,8 @@ unifiedPlatforms = function(mergedResults) {
 };
 
 organizeMergedResults = function(mergedResults) {
-	var content = '';
-	content += '<table>';
+	var content = '<table>';
+
 	content += '<tr><th>Platform</th><th>Name</th><th>Category</th><th>Address</th><th>Phone</th><th>Url</th><th>Website</th><th>Image</th></tr>';
 	_.forEach(mergedResults, function(i) {
 
@@ -546,6 +418,7 @@ organizeMergedResults = function(mergedResults) {
 		content +=	'</tr>';
 	});
 	content += '</table>';
+
 	return content;
 };
 
@@ -793,14 +666,6 @@ getLongitude = function(data) {
 
 getRadius = function(data) {
 	return data.searchParams.address.geometry.radius = Number($('#radius').val());
-};
-
-formatAddress = function(addressObj) {
-	var address = '';
-	_.forIn(addressObj, function(i) {
-		adress += addressObj.street + ' ' + addressObj.city + ' ' + addressObj.state + ' ' + addressObj.country + ' ' + addressObj.zipcode;
-	});
-	return address;
 };
 
 $('.request').on('click',function() {
